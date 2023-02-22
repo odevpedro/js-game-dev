d33fcc91398877b06e98ed7cd663a319cf709bcd
odevpedro <pedrosschmidt2@gmail.com>
2023-02-22 16:14:20 -0300

    complete models

--- script.js
+++ script.js
@@ -5,4 +5,26 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1280;
 canvas.height = 720;
 
+class Player {
+    constructor(game){
+        this.game = game;
+    }
+}
+
+    class Game {
+        constructor(canvas){
+            this.canvas = canvas;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.Player = new Player(this);
+    }
+}
+
+const game = new Game(canvas);
+console.log(game)
+
+    function animate(){
+
+    }
+
 });
